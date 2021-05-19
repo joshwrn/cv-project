f4f033952281a8cf57821b065325c483672d8c14
joshwrn <joshnwarren@gmail.com>
2021-05-19 16:44:59 -0700

    remade with functional components

--- src/App.js
+++ src/App.js
@@ -4,18 +4,13 @@ import Fill from './components/form/FullForm';
 import Results from './components/Results';
 import styles from './styles/styles.css';
 
-class App extends Component {
-  constructor() {
-    super();
-  }
-  render() {
-    return (
-      <div className="App">
-        <h2 className="main-header">CV Form</h2>
-        <FullForm />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <h2 className="main-header">CV Form</h2>
+      <FullForm />
+    </div>
+  );
 }
 
 export default App;
--- src/components/form/FullForm.js
+++ src/components/form/FullForm.js
@@ -1,86 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Fill from './Fill';
 import Results from '../Results';
 
-class FullForm extends Component {
-  constructor() {
-    super();
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleEdit = this.handleEdit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+const initial = {
+  name: '',
+  email: '',
+  phone: '',
+  school: '',
+  study: '',
+  date: '',
+  company: '',
+  position: '',
+  tasks: '',
+  start: '',
+  end: '',
+};
 
-    this.state = {
-      visible: true,
-      itemObject: {
-        name: '',
-        email: '',
-        phone: '',
-        school: '',
-        study: '',
-        date: '',
-        company: '',
-        position: '',
-        tasks: '',
-        start: '',
-        end: '',
-      },
-    };
-  }
+function FullForm() {
+  const [visible, setVisible] = useState(true);
+  const [itemObject, setItems] = useState(initial);
 
-  handleChange(e) {
+  function handleChange(e) {
     e.preventDefault();
     const { name, value } = e.target;
-    // you have to wrap in brackets to describe string inside object property
-    // this.setState({ [e.target.name]: e.target.value }); //idk if this.state is necessary
-    this.setState((prevState) => {
-      return {
-        itemObject: {
-          ...prevState.itemObject,
-          [name]: value,
-        },
-      };
+    setItems({
+      ...itemObject,
+      [name]: value,
     });
   }
 
-  handleSubmit(e) {
-    // using conditionals instead of separate handles caused lag and bugs
+  function handleSubmit(e) {
     e.preventDefault();
-    this.setState({
-      visible: false,
-    });
+    setVisible(false);
   }
 
-  handleEdit(e) {
+  function handleEdit(e) {
     e.preventDefault();
-    this.setState({
-      visible: true,
-    });
+    setVisible(true);
   }
 
-  render() {
-    return (
-      <div>
-        <form
-          className="form"
-          onSubmit={this.state.visible ? this.handleSubmit : this.handleEdit}
-        >
-          {this.state.visible ? (
-            <div>
-              <Fill onChange={this.handleChange} item={this.state.itemObject} />
-            </div>
-          ) : (
-            <Results item={this.state.itemObject} />
-          )}
-          <button
-            className="submit-button"
-            type={this.state.visible ? 'submit' : 'edit'}
-          >
-            {this.state.visible ? 'Submit Form' : 'Edit Form'}
-          </button>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <form className="form" onSubmit={visible ? handleSubmit : handleEdit}>
+        {visible ? (
+          <div>
+            <Fill onChange={handleChange} item={itemObject} />
+          </div>
+        ) : (
+          <Results item={itemObject} />
+        )}
+        <button className="submit-button" type={visible ? 'submit' : 'edit'}>
+          {visible ? 'Submit Form' : 'Edit Form'}
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default FullForm;
